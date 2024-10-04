064d4ae445e75eda1f36c741139a87aefc5ba062
gmedyacine <gmedyacine@yahoo.fr>
2024-10-04 15:28:50 +0200

    Update wk-redis.py

--- wk-redis.py
+++ wk-redis.py
@@ -1,17 +1,41 @@
+import requests
 import redis
+import json
+import os
 
-# Remplacez ces valeurs par celles récupérées de Vault
-redis_host = 'REDIS_HOST'  # Remplacez par l'adresse IP ou le nom de domaine de votre serveur Redis
-redis_port = 6379  # Port par défaut de Redis, changez-le si votre configuration est différente
-redis_user = 'USER_RETRIEVED_FROM_VAULT'
-redis_password = 'PASSWORD_RETRIEVED_FROM_VAULT'
+# Configuration des variables d'environnement ou remplacer par des valeurs statiques sécurisées
+VAULT_ADDR = os.getenv('VAULT_ADDR', 'https://vault.staging.echonen.com')
+VAULT_TOKEN = os.getenv('VAULT_TOKEN', 'your-vault-token')
+REDIS_HOST = os.getenv('REDIS_HOST', 'redis-host')
 
-# Connexion à Redis
-r = redis.Redis(host=redis_host, port=redis_port, username=redis_user, password=redis_password, decode_responses=True)
+def get_redis_credentials(vault_url, vault_token):
+    """Récupère les identifiants Redis depuis Vault."""
+    headers = {'X-Vault-Token': vault_token}
+    user_path = f"{vault_url}/v1/UPM_CARDIF/8785/EC002102860/cloud/data/redis/RDO021000358"
+    pass_path = f"{vault_url}/v1/UPM_CARDIF/8785/EC002102860/cloud/data/redis/RDO021000358"
 
-# Test de la connexion
-try:
-    response = r.ping()
-    print("Redis is connected:", response)
-except Exception as e:
-    print("Failed to connect to Redis:", e)
+    # Récupération du nom d'utilisateur
+    response = requests.get(user_path, headers=headers)
+    redis_user = json.loads(response.text)['data']['data']['user']
+
+    # Récupération du mot de passe
+    response = requests.get(pass_path, headers=headers)
+    redis_password = json.loads(response.text)['data']['data']['password']
+
+    return redis_user, redis_password
+
+def test_redis_connection(redis_host, redis_user, redis_password):
+    """Teste la connexion à Redis."""
+    try:
+        r = redis.Redis(host=redis_host, username=redis_user, password=redis_password, decode_responses=True)
+        ping_response = r.ping()
+        print("Redis is connected:", ping_response)
+    except Exception as e:
+        print("Failed to connect to Redis:", e)
+
+def main():
+    redis_user, redis_password = get_redis_credentials(VAULT_ADDR, VAULT_TOKEN)
+    test_redis_connection(REDIS_HOST, redis_user, redis_password)
+
+if __name__ == "__main__":
+    main()
