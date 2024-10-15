9102f8ca94ab3c7960019206796cea8e4ea9635c
gmedyacine <gmedyacine@yahoo.fr>
2024-10-15 11:51:06 +0200

    Update wk-redis.py b64

--- wk-redis.py
+++ wk-redis.py
@@ -1,6 +1,5 @@
 import requests
-import json
-import redis
+import base64
 
 def get_secret_from_vault(secret_path, vault_token, vault_url, vault_namespace):
     # Construction de l'URL de l'API
@@ -22,50 +21,29 @@ def get_secret_from_vault(secret_path, vault_token, vault_url, vault_namespace):
     else:
         raise Exception(f"Failed to retrieve secret: {response.status_code} {response.text}")
 
-def test_redis_connectivity(redis_host, redis_port, redis_user, redis_password):
-    # Connexion à Redis
-    r = redis.Redis(
-        host=redis_host,
-        port=redis_port,
-        username=redis_user,
-        password=redis_password,
-        decode_responses=True
-    )
-    
-    try:
-        # Test de connectivité via PING
-        response = r.ping()
-        if response:
-            print("Successfully connected to Redis")
-        else:
-            print("Failed to connect to Redis")
-    except Exception as e:
-        print(f"Error connecting to Redis: {e}")
-
-# Exemple d'utilisation
+# Exemple d'utilisation pour récupérer le certificat
 vault_url = "https://vault.staging.echonet.com"
 vault_token = "votre_token_vault"
 vault_namespace = "UPM_CARDIF/8785/EC002102860"
 secret_path = "cloud/data/redis/RDO021000358"
-redis_host = "redis_host_address"  # Remplacez par l'adresse de votre serveur Redis
-redis_port = 6379  # Le port Redis par défaut est 6379
 
 try:
-    # Récupération des secrets depuis Vault
     secret_data = get_secret_from_vault(secret_path, vault_token, vault_url, vault_namespace)
     
-    # Extraction du mot de passe et de l'utilisateur Redis depuis la réponse JSON
-    redis_user = secret_data['data']['data']['user']
-    redis_password = secret_data['data']['data']['password']
-    certificate = secret_data['data']['data']['certificate']
-    with open("redis_certificate.pem", "w") as cert_file:
-        cert_file.write(certificate)
+    # Récupérer le certificat encodé en Base64
+    certificate_base64 = secret_data['data']['data']['certificate']
     
-    # Test de la connectivité à Redis
-    test_redis_connectivity(redis_host, redis_port, redis_user, redis_password)
+    # Décodage du certificat Base64 en binaire
+    certificate_decoded = base64.b64decode(certificate_base64)
+
+    # Ajouter les balises BEGIN et END autour du certificat
+    pem_certificate = f"-----BEGIN CERTIFICATE-----\n{certificate_base64}\n-----END CERTIFICATE-----"
+
+    # Sauvegarder le certificat décodé dans un fichier
+    with open("/tmp/redis_certificate.pem", "w") as cert_file:
+        cert_file.write(pem_certificate)
+
+    print("Certificat décodé et sauvegardé dans /tmp/redis_certificate.pem")
 
 except Exception as e:
     print(str(e))
-
-
-
