42155805ca328673e062ea17239eda199399d6e3
gmedyacine <gmedyacine@yahoo.fr>
2024-10-25 13:43:09 +0100

    Create curl-cos.sh

---
+++ curl-cos.sh
@@ -0,0 +1,3 @@
+curl -v -H "Authorization: HMAC <HMAC_ACCESS_KEY>" \
+     -H "x-amz-content-sha256: UNSIGNED-PAYLOAD" \
+     "https://bu0021009393.s3.direct.eu-fr2.cloud-object-storage.appdomain.cloud:4229"
