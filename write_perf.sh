eb74829c6c24582b4a469054742abf7ff195020d
gmedyacine <gmedyacine@yahoo.fr>
2025-10-28 15:02:39 +0100

    Add pip-audit for security auditing of dependencies

--- write_perf.sh
+++ write_perf.sh
@@ -4,3 +4,8 @@ export PIP_TRUSTED_HOST="repo.artifactory-dogen.group.echonet"
 pip-compile --generate-hashes --resolver=backtracking \
   --pip-args="--only-binary=:none:" \
   -o requirements.txt requirements.in
+
+python -m pip install --no-cache-dir pip-audit
+pip-audit -r requirements.in -f cyclonedx-json -o gl-sbom-report.cdx.json
+
+pip-audit -r requirements.in -f sarif -o gl-sast-report.sarif
